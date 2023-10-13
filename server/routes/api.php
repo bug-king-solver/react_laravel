604606a7569a75ab6cb92cd9ad93e37de535ffe7
jannydream0826 <olek.dream.high@gmail.com>
2023-10-14 04:22:09 +0900

    backend: define api and confroller to export as csv and xml

--- server/routes/api.php
+++ server/routes/api.php
@@ -17,4 +17,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::get('/export/csv', 'ExportController@exportCSV');
+Route::get('/export/xml', 'ExportController@exportXML');
+
 Route::resource('books', 'BookController');
