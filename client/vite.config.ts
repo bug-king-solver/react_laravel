c2822ba034ac8b8aa315e65fd9b86983f459157f
jannydream0826 <olek.dream.high@gmail.com>
2023-10-14 02:57:05 +0900

    frontend: add dependencies and fix cors error

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+// import proxy from 'vite-plugin-proxy';
+
+// Define your backend server URL
+const backendServerUrl = 'http://localhost:8080';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react()], // Add this line
+  server:{
+    proxy:{
+      '/api':{
+        target: backendServerUrl,
+        changeOrigin: true,
+        // rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    }
+  }
+});
